fcf7e0a07376d11f97d1100e1f0d612c94e60bac
wawanpi <muhaguskurniawan@gmail.com>
2023-06-19 15:55:59 +0700

    membuka file dalam mode menulis

--- StreamReaderWriter_202/StreamReaderWriter_202.cpp
+++ StreamReaderWriter_202/StreamReaderWriter_202.cpp
@@ -1,3 +1,4 @@
+//ioFile
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -31,4 +32,20 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
-	
+	cout << endl << ">= membuka dan membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}
