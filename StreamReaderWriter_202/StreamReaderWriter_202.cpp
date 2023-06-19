90b2c255c60fd7261f1a38a48df805f4c8eb38ec
wawanpi <muhaguskurniawan@gmail.com>
2023-06-19 15:47:20 +0700

    selesai dalam menulis sekarang tutup filenya

--- StreamReaderWriter_202/StreamReaderWriter_202.cpp
+++ StreamReaderWriter_202/StreamReaderWriter_202.cpp
@@ -23,4 +23,28 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
-	
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= membuka dan membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}
