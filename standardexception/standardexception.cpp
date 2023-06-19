78373e79e2db1e272a20598b7199976a5251cad5
wawanpi <muhaguskurniawan@gmail.com>
2023-06-20 00:03:38 +0700

    .

--- standardexception/standardexception.cpp
+++ standardexception/standardexception.cpp
@@ -9,5 +9,17 @@ int main()
 {
 	cout << "awal program" << endl; //penanda 1:...
 	try {
-		
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		cout << data.at(4) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		// akan dieksekusi karena array data hanya memiliki 3 elemen /
+	}
+	cout << "Baris program yang terakhir" << endl;
+	// penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan /
+		return 0;
 }
