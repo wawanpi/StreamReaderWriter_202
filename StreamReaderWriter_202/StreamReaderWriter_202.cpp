434a9b2e275df0920f28113b8fda5e7cbbfc46bd
wawanpi <muhaguskurniawan@gmail.com>
2023-06-19 15:47:03 +0700

    menulis dan memasukan nilai dari 'baris' ke dalam file

--- StreamReaderWriter_202/StreamReaderWriter_202.cpp
+++ StreamReaderWriter_202/StreamReaderWriter_202.cpp
@@ -20,4 +20,7 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
-		
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	
