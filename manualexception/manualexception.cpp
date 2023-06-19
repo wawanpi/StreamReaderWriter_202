5b3d30bf629755ed7b5e9751a3d8d7f41ccdc91c
wawanpi <muhaguskurniawan@gmail.com>
2023-06-19 23:21:59 +0700

    done

--- manualexception/manualexception.cpp
+++ manualexception/manualexception.cpp
@@ -8,4 +8,13 @@ int main()
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
-} 
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		// jika selain integer maka blok ini akan dieksekusi 
+			cout << "default pengecualian dieksekusi" << endl;
+	}
+	return 0;
+}
