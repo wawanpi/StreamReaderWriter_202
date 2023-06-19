20f2f68494c01cbe784777e7250501453ebc586e
wawanpi <muhaguskurniawan@gmail.com>
2023-06-19 15:46:30 +0700

    loop akan berhenti jika anda memasukan karakter q

--- StreamReaderWriter_202/StreamReaderWriter_202.cpp
+++ StreamReaderWriter_202/StreamReaderWriter_202.cpp
@@ -18,5 +18,6 @@ int main() {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
 		
-}
